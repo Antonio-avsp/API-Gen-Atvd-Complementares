e30b20b21a4eb1f9c031d5c53991d1c2ec7a7aa8
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-04-07 14:19:10 -0300

    permissões de regras

--- src/main/java/com/pi/apigenatvdcomplementares/config/SecurityConfig.java
+++ src/main/java/com/pi/apigenatvdcomplementares/config/SecurityConfig.java
@@ -86,6 +86,10 @@ public class SecurityConfig {
                                                 .hasAnyRole("SUPER_ADMIN", "COORDENADOR")
                                                 .requestMatchers(HttpMethod.GET, "/turmas", "/turmas/**")
                                                 .authenticated()
+                                                // Gestão de Regras de Atividade
+                                                .requestMatchers("/regras", "/regras/**")
+                                                .hasAnyRole("SUPER_ADMIN", "COORDENADOR")
+
                                                 // Gestão de Coordenadores (Ajuste para a tela AdminCoordinators)
                                                 .requestMatchers("/coordenadores-cursos/**").hasRole("SUPER_ADMIN")
 
